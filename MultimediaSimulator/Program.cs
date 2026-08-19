bd88cfc8a5ee1652492d279349595bf3a3fe1cd1
bytor <bytor@gmail.com>
2026-08-19 13:08:34 +0300

    Wire up the UAV streaming service and its configuration.
    Registers IUavStreamService/FfmpegUavStreamService as a singleton
    (required since it holds the in-memory dictionary of running ffmpeg
    processes, which must stay a single shared instance across requests)
    and binds the Streaming config section to StreamingOptions.

--- MultimediaSimulator/Program.cs
+++ MultimediaSimulator/Program.cs
@@ -1,3 +1,5 @@
+using MultimediaSimulator.Streaming;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.Configure<StreamingOptions>(builder.Configuration.GetSection(StreamingOptions.SectionName));
+builder.Services.AddSingleton<IUavStreamService, FfmpegUavStreamService>();
+
 
 var app = builder.Build();
 
